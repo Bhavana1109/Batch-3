3be66b6bb1996a3e77ed1782fce8225dfc02892e
Bhavana1109 <yelagandalabhavana@gmail.com>
2025-01-06 13:58:00 +0530

    pushing files to repo

---
+++ app.js
@@ -0,0 +1,56 @@
+//console.log(10);
+//document.writeln(1000);
+/**var a=10;
+a="HI";//re-initialization
+console.log(a);
+
+var b=1000;
+var b=2000;
+console.log(b);**/
+
+/**let a=10;
+a=100;//re-initialization is possible
+console.log(a);
+
+let b=10;
+let b=20;
+console.log(b); re-declatation not possible**/
+
+/**const dob=1990;
+dob=2000;      re-initialization is  not possible
+console.log(dob);
+
+const dob=1990;
+const dob=2000;
+console.log(dob); re-declaration not possible**/
+
+//"let str1="doubleqoutes"
+//console.log(str1)
+console.log(typeof(str1));
+
+
+let str2='singleqoutes';
+console.log(str2);
+console.log(typeof(str2));
+
+let str3=`template literals`;
+console.log(str3);
+console.log(typeof(str3));
+
+let num1=10;
+console.log(num1, typeof(num1));
+
+let num2=10466.867;
+console.log(num2, typeof(num2));
+
+let bool1=true;
+console.log(bool1,typeof(bool1));
+
+let a;
+console.log(a,typeof(aS));
+
+let b=null;
+console.log(a);
+console.log(typeof(null))
+
+console.log(2n,typeof(2n));
