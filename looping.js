663b001a3b9297cdaef457c6c7848ca21127eb16
Bhavana1109 <yelagandalabhavana@gmail.com>
2025-01-08 10:27:33 +0530

    today work

---
+++ looping.js
@@ -0,0 +1,30 @@
+
+//for(let i=1;i<=5;i++){
+//    console.log("Good Morning")
+//}
+
+for(let i=1;i<=100;i++){
+    if(i%2==0){
+        console.log(i+" is a even number");
+    }
+    else{
+        console.log(i+" is a odd number");
+    }
+}
+let i=1;
+while(i<=5){
+    console.log("Good Morning");
+    i++;
+}
+
+let j=1
+let n=5;
+do{
+    j++;
+    console.log("Good Morning")
+}
+while(j<=n)
+
+function addToCart(){
+    alert("Product is added")
+}
