eb18d63710074b2db77132d219ab3106e32ce5c4
Bhavana1109 <yelagandalabhavana@gmail.com>
2025-01-10 17:03:08 +0530

    pushing files to repo

--- functions.js
+++ functions.js
@@ -1,41 +1,117 @@
-//! function without parameters
-function demo(){
-    console.log("function without parameters");
-}
-demo();
+// function addToCart(){
+//     alert("Product is added to cart")
+// }
 
-//! function with parameters
-function userDetails(name,age,city){
-    console.log(name);
-    console.log(age);
-    console.log(city);
-    
-}
-userDetails("Bhavana",20,"Nizamabad");
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10 , 40)
+// add(30 , 40)
+// add(50 , 40)
+// add(100 , 40)
+// add(13 , 50)
 
-//!Anonymous function
-//function(){
- //   console.log("Anonymous function");
+// ! function without parameters
+// function demo(){
+//     console.log("function without parameters");
+// }
+// demo();
+
+// ! function with parameters
+
+// function userDetails(name , age , city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// userDetails("Yeshwant" , 20 , "Hyderbad");
+
+//! anonymus function
+// function(){
+//     console.log("Anonymus function");
+// }
+// ();
+
+// ! function  expression
+// let x = function(){
+//     console.log("function expression is executing");
+// };
+// x();
+
+
+// ! IIFE
+// (function(){
+//     console.log("IIFE");
+// })();
+
+//! arrow function
+// function demo() 
+// {
+//     console.log("Hello");
+// }
+// demo();
+
+// let x = ()=>console.log("Arrow function");
+// x();
+
+// let x =_=>console.log("Arrow function");
+// x();
+
+// let x =a=>console.log(a);
+// x(5);
+
+// let x =(a , b)=>console.log(a+b);
+// x(5,5);
+// let x=_=>{console.log("Hi");
+//         console.log("Hello");
+//         console.log("Bye");}
+//     x();
+
+// return
+//function demo(a,b){//explicit return
+//    return a+b;
+//};
+//let x=demo(5,5);
+//console.log(x);
+
+//let y=(a,b)=>a+b;a//implicit return
+//let a=y(10,20);
+//console.log(a);
+
+//let z = (a,b)=>{return a+b};//explicit return
+//let b=z(10,100);
+//console.log(b);
+
+//function demo(a,b){
+//   console.log("Hi");
+//    console.log("Hello");
+//    return a,b;
 //}
-//();
 
-//!function expression
-let x=function(){
-    console.log("function expression")
-}
+//Higher order function
+//function hof(a){
+//  return a();
+//};
+//let x=hof(function(){return "Hello i am a callback functon"});
+//console.log(x);
+
+//function hof(a,b){
+//    return a(10,5);
+//};
+//let y=hof(function(a,b){return a+b});
+//console.log(y);
+
+//closure
+var a=100;
+let b="hi";
+function x(){
+    var user="bhavana";
+    let company="Microsoft";
+    const sal=12345678;
+    console.log(user);
+    console.log(sal);
+    console.log(company);
+    console.log(a,b);
+};
 x();
 
-//IIFE
-(function(){
-    console.log("IIFE");
-})();
-
-//arrow function
-//let y = _=> console.log("Arrow function");
-//y();
-
-let y=_=>{console.log("HI");
-    console.log("Hello");
-    console.log("Bye");
-}
-y();
--- patterns.js
+++ patterns.js
@@ -5,16 +5,22 @@
 // * * * * *
 // * * * * *
 // Number of rows and columns for the pattern
-const rows = 5;
-const cols = 5;
-for (let i = 0; i < rows; i++) {
-    let rowPattern = "";
-    for (let j = 0; j < cols; j++) {
-        rowPattern += "* ";
-    }
-    console.log(rowPattern.trim()); // Print the row
-    }
+//const rows = 5;
+//const cols = 5;
+//for (let i = 0; i < rows; i++) {
+    //let rowPattern = "";
+    //for (let j = 0; j < cols; j++) {
+        //rowPattern += "* ";
+    //}
+    //console.log(rowPattern.trim()); // Print the row
+    //}
 
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        document.writeln("*");
+    }
+    document.write("<br>")
+}
 
 //2Q
 //*
@@ -23,11 +29,23 @@ for (let i = 0; i < rows; i++) {
 //* * * *
 //* * * * *
 
-let n = 5;
-for (let i = 1; i <= n; i++) {
-    let str = "* ";
-    console.log(str.repeat(i));
+//let n = 5;
+//for (let i = 1; i <= n; i++) {
+//    let str = "* ";
+//    console.log(str.repeat(i));
+//}
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        if(i>=j){
+            document.writeln("*");
+        }else{
+            document.writeln("&nbsp;&nbsp;");
+        }    
+    }
+    document.write("<br>")
 }
+
 
 //3Q
 //*   *
@@ -36,16 +54,28 @@ for (let i = 1; i <= n; i++) {
 // * * 
 //*   *
 
-const size = 5; // Size of the X pattern (must be odd)
+//const size = 5; // Size of the X pattern (must be odd)
 
-for (let i = 0; i < size; i++) {
-    let row = "";
-    for (let j = 0; j < size; j++) {
-        if (j === i || j === size - i - 1) {
-        row += "*";
-        } else {
-            row += " ";
-        }
+//for (let i = 0; i < size; i++) {
+//    let row = "";
+//   for (let j = 0; j < size; j++) {
+//        if (j === i || j === size - i - 1) {
+//        row += "*";
+//        } else {
+//            row += " ";
+//        }
+//    }
+//    console.log(row);
+//}
+
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        if(i==j || i+j==6){
+            document.writeln("*");
+        }else{
+            document.writeln("&nbsp;&nbsp;&nbsp;");
+        }    
     }
-    console.log(row);
+    document.write("<br>")
 }
