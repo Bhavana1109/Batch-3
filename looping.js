9fab1ebe8907428f66a73a3becee3621e1a0a713
Bhavana1109 <yelagandalabhavana@gmail.com>
2025-01-11 12:33:32 +0530

    pushing files to repo

--- looping.js
+++ looping.js
@@ -2,6 +2,52 @@
 //for(let i=1;i<=5;i++){
 //    console.log("Good Morning")
 //}
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+
+
+// for(let i=1;i<=5;i++)
+// {
+// console.log("Hello world!");
+// }
+// let n = 100;
+// for(let i=1;i<=n;i++){
+//     //console.log(i);
+//     if(i%2 == 0){
+//         console.log(i + " is a even number");
+//     }else{
+//         console.log(i+" is a odd number");
+//     }
+// }
+
+//! WAJP to find a factorial of a number
+//! 5 = 120 ; 5*4*3*2*1
+// let num = 5;
+// let fact = 1;
+// for(let i=1 ; i<=num;i++){
+//     // console.log(i);
+//     fact = fact*i;
+// };
+// console.log(fact);
+
+// ! while loop
+// let i = 1;
+
+// while(i<=5){
+//     console.log("Good Morning!!!");
+//     i++;
+// }
+
+// ! do-while
+// let i = 1;
+// do{
+//     console.log("Please wake up and interact!!!!!!");
+//     i++;
+// }while(i >= 5);
+
 
 for(let i=1;i<=100;i++){
     if(i%2==0){
