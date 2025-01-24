bc16edc83828b1c03c74569ddf5cb6d96ff0d3b1
Bhavana1109 <yelagandalabhavana@gmail.com>
2025-01-24 12:57:26 +0530

    pushing files to repo

--- dom.js
+++ dom.js
@@ -1,1 +1,34 @@
-console.log(welcome)
+// console.log(window);
+// console.log(document);
+
+// let element = document.getElementById("demo");
+// element.innerText = "G+"
+// console.log(element);
+
+//let ele = document.getElementById("test");
+// ele.innerText= "<h1>Header</h1>"
+//ele.innerHTML = "<h1>Header</h1>"
+//console.log(ele);
+
+/*let ele= document.getElementsByClassName("test");
+console.log(ele);
+console.log(Array.isArray(ele));
+ele[0].innerTest="Hello"
+//ele[0].style.backgroundColor="Tomato"
+
+let x=[...ele];
+x.map(element=>{
+    element.style.backgroundColor="orange"
+})*/
+
+let ele=document.getElementsByName("");
+console.log(ele);
+
+let ele1=document.querySelector("#mru");
+console.log(ele1);
+
+let ele2=document.querySelector(".test");
+console.log(ele2);
+
+let ele3=document.querySelectorAll(".test");
+console.log(ele3);
