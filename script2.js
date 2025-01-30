89575c39c41c441935f9e8ac71d42e4e22ba1a1d
Bhavana1109 <yelagandalabhavana@gmail.com>
2025-01-30 19:03:21 +0530

    Today assignment is done

--- script2.js
+++ script2.js
@@ -1,3 +1,5 @@
-//import { collegeName } from "./script1.js";
-import { Tasking as tasking } from "./script1.js";
+//import  {collegeName}  from "./script1.js";
+import total, { Tasking as tasking } from "./script1.js";
 tasking();
+console.log(total);
+ 
